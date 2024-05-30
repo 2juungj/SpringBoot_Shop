c4a41da951caa46cde2012a05b64137900b84941
2juungj <y2k2211@naver.com>
2024-05-30 16:14:55 +0900

    @Controller -> @RestController

--- src/main/java/com/cos/blog/controller/api/ItemApiController.java
+++ src/main/java/com/cos/blog/controller/api/ItemApiController.java
@@ -3,20 +3,19 @@ package com.cos.blog.controller.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.Item;
 import com.cos.blog.service.ItemService;
 
-@Controller
+@RestController
 public class ItemApiController {
 
 	@Autowired
@@ -24,7 +23,6 @@ public class ItemApiController {
 
 	// 상품 등록 (POST)
 	@PostMapping("/seller/new/item")
-	@ResponseBody
 	public ResponseDto<Integer> itemSave(@RequestBody Item item, @AuthenticationPrincipal PrincipalDetail principal) {
 		itemService.상품등록(item, principal.getUser());
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
@@ -32,7 +30,6 @@ public class ItemApiController {
 
 	// 상품 수정 (PUT)
 	@PutMapping("/seller/update/item/{id}")
-	@ResponseBody
 	public ResponseDto<Integer> itemUpdate(@RequestBody Item item, @PathVariable int id) {
 		itemService.상품수정(item, id);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
@@ -40,7 +37,7 @@ public class ItemApiController {
 	
 	// 상품 삭제 (DELETE)
 	@DeleteMapping("/seller/delete/{id}")
-	public String itemDelete(@PathVariable("id") int id) {
+	public String itemDelete(@PathVariable int id) {
 		itemService.상품삭제(id);
 		return "/";
 	}
